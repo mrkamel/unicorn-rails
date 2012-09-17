2e3b728080bf3c0f3133178c1370df6370f618f4
Benjamin Vetter <vetter@flakks.com>
2012-09-17 20:59:35 +0200

    Syntax fixes.

--- config/unicorn/unicorn.rb
+++ config/unicorn/unicorn.rb
@@ -21,7 +21,7 @@ before_fork do |server, worker|
 
   if File.exists?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      Process.kill "QUIT", File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
       # Someone else already did the job for us.
     end
